0ea8a7aa692e90d9331512f4bf5a4eef360da55e
dwbergstrom <dwbergstrom@gmail.com>
2018-11-30 14:33:17 -0500

    Update Finish component to index on load
    + load finished rides on clicking to /finishes
    + add "loading" message to finish and ride components

--- src/finishes/components/Finishes.js
+++ src/finishes/components/Finishes.js
@@ -4,10 +4,52 @@ import axios from 'axios'
 
 class Finishes extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      finishes: []
+    }
+  }
+
+  async componentDidMount() {
+    const token = this.props.user.token
+
+    console.log(token)
+
+    const response = await axios.get('http://localhost:4741/finishes', { headers: { Authorization: `Bearer ${token}` } })
+    this.setState({finishes: response.data.finishes})
+
+  }
+
+  render() {
+    let individualFinish
+    const { finishes } = this.state
+
+    if (finishes.length === 0) {
+      individualFinish = <p>Loading</p>
+    } else {
+      individualFinish = finishes.map(finish => {
+        const { id, notes, date, duration } = finish
+        const points = finish.ride.points
+        const name = finish.ride.name
+        return (
+          <React.Fragment key={id}>
+            <h3>Ride: {name}</h3>
+            <p>Notes: {notes}</p>
+            <p>Date: {date}</p>
+            <p>Duration: {duration}</p>
+            <p>Points: {points}</p>
+          </React.Fragment>
+        )
+      })
+    }
     return (
-      <h1>Finishes</h1>
+      <React.Fragment>
+        <h2 className="user-stats">My stats</h2>
+        <h1>My completed rides</h1>
+        {individualFinish}
+      </React.Fragment>
     )
   }
 }
--- src/rides/components/Rides.js
+++ src/rides/components/Rides.js
@@ -18,34 +18,33 @@ class Rides extends React.Component {
     const response = await axios.get('http://localhost:4741/rides')
     this.setState({rides: response.data.rides})
 
-    // axios.get('http://localhost:4741/rides')
-    //   .then( response => this.setState({rides: response.data.rides}))
-    //   .catch(console.error)
   }
 
   render() {
 
+    let individualRide
     const { rides } = this.state
 
-    const individualRide = rides.map(ride => {
-      const { id, name, photo_url, description, distance, points } = ride
+    if (rides.length === 0) {
+      individualRide = <p>Loading</p>
+    } else {
+      individualRide = rides.map(ride => {
+        const { id, name, photo_url, description, distance, points } = ride
 
-      return (
-        <React.Fragment key={id}>
-          <h1>{name}</h1>
-          <p>{description}</p>
-          <p>Distance: {distance}</p>
-          <p>Points: {points}</p>
-          <br />
-        </React.Fragment>
-
-      )
-
-
-    })
-
+        return (
+          <React.Fragment key={id}>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <p>Distance: {distance}</p>
+            <p>Points: {points}</p>
+            <br />
+          </React.Fragment>
+        )
+      })
+    }
     return (
       <React.Fragment>
+        <h2 className="leaderboard">Leaderboard</h2>
         <h1>Rides</h1>
         {individualRide}
       </React.Fragment>
