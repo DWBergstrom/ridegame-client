603a70f48e92c3b5c50707033e611b2117b08314
dwbergstrom <dwbergstrom@gmail.com>
2018-11-30 14:33:17 -0500

    Added logic to render current user's finishes only
    + changed Finish controller in API to inherit from OpenReadController
    + changed index action for Finish controller to output all finishes
    + added logic to Finishes component on front-end to render just the
    current user's rides and stats
    + added renderRun variable to ensure stats don't errantly update if
    user clicks same link again

--- src/finishes/components/Finishes.js
+++ src/finishes/components/Finishes.js
@@ -17,10 +17,12 @@ class Finishes extends React.Component {
       finishes: []
     }
 
+    // declare variables for stats aside
     this.totalDistance = 0
     this.totalRides = 0
     this.totalTime = 0
     this.totalPoints = 0
+    this.renderRun = 0
 
     this.changeHandler = this.changeHandler.bind(this)
 
@@ -31,30 +33,28 @@ class Finishes extends React.Component {
 
     const response = await axios.get(`${apiUrl}` + '/finishes', { headers: { Authorization: `Bearer ${token}` } })
     this.setState({finishes: response.data.finishes})
-
   }
 
   changeHandler() {
     this.componentDidMount()
   }
 
-
-
   render() {
     let individualFinish
     const { finishes } = this.state
-    const  user = this.props.user
+    const  currentUser = this.props.user
 
     if (finishes.length === 0) {
       individualFinish = <Link to="/rides">Add some rides to your finished stats!</Link>
     } else {
 
       individualFinish = finishes.map(finish => {
-        const { id, notes, date, duration } = finish
+        const { id, notes, date, duration, user } = finish
         const points = finish.ride.points
         const name = finish.ride.name
         const distance = finish.ride.distance
         const ride_id = finish.ride.id
+        const finishUser = user
         const changeProps = {
           user: this.props.user,
           id: id,
@@ -65,40 +65,53 @@ class Finishes extends React.Component {
           distance: distance
         }
 
-        // recalc totals
-        this.totalDistance += distance
-        this.totalRides += 1
-        this.totalTime += duration
-        this.totalPoints += points
+        if (finishUser.id === currentUser.id) {
+          this.totalDistance += distance
+          this.totalRides += 1
+          this.totalTime += duration
+          this.totalPoints += points
 
-        const detail_link = {
-          pathname: `/finishes/${id}`,
-          finishParams: {id, name, notes, date, duration, ride_id, points, user, distance}
+          const detail_link = {
+            pathname: `/finishes/${id}`,
+            finishParams: {id, name, notes, date, duration, ride_id, points, currentUser, distance}
+          }
+          return (
+            <div className="finishes-div" key={id}>
+              <h3><Link to={detail_link} user={currentUser} replace>Ride: {name}</Link></h3>
+              <p>Notes: {notes}</p>
+              <p>Date: {date}</p>
+              <p>Duration: {duration}</p>
+              <p>Distance: {distance}</p>
+              <p>Points: {points}</p>
+              <FinishDelete changeHandler={this.changeHandler} {...changeProps}/>
+              <br />
+              <FinishUpdate changeHandler={this.changeHandler} {...changeProps}/>
+            </div>
+          )
         }
-        return (
-          <div className="finishes-div" key={id}>
-            <h3><Link to={detail_link} user={user} replace>Ride: {name}</Link></h3>
-            <p>Notes: {notes}</p>
-            <p>Date: {date}</p>
-            <p>Duration: {duration}</p>
-            <p>Distance: {distance}</p>
-            <p>Points: {points}</p>
-            <FinishDelete changeHandler={this.changeHandler} {...changeProps}/>
-            <br />
-            <FinishUpdate changeHandler={this.changeHandler} {...changeProps}/>
-          </div>
-        )
       })
+      if (this.totalDistance === 0) {
+        individualFinish = <Link to="/rides">Add some rides to your finished stats!</Link>
+      }
+      this.renderRun += 1
+      console.log(this.renderRun)
+      if (this.renderRun > 1) {
+        this.totalDistance /= this.renderRun
+        this.totalRides /= this.renderRun
+        this.totalTime /= this.renderRun
+        this.totalPoints /= this.renderRun
+        this.renderRun -= 1
+      }
     }
 
     return (
       <React.Fragment>
         <div className="stats-aside">
           <h2>My stats</h2>
-          <p>Distance: {this.totalDistance / 2} miles</p>
-          <p>Time: {(this.totalTime / 2 / 60).toFixed(1)} hours</p>
-          <p>Rides: {this.totalRides / 2}</p>
-          <p>Points: {this.totalPoints / 2}</p>
+          <p>Distance: {this.totalDistance} miles</p>
+          <p>Time: {(this.totalTime / 60).toFixed(1)} hours</p>
+          <p>Rides: {this.totalRides}</p>
+          <p>Points: {this.totalPoints}</p>
         </div>
         <div className="user-finishes">
           <h1>My completed rides</h1>
@@ -107,6 +120,7 @@ class Finishes extends React.Component {
       </React.Fragment>
     )
   }
+
 }
 
 export default Finishes
