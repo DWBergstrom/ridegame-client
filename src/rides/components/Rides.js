c5166b1a82665ed22e6c2d4a92702fce37b65529
dwbergstrom <dwbergstrom@gmail.com>
2018-12-06 10:14:16 -0500

    Adjust maps to use correct iframe URLs
    + update map "photo_url"

--- src/rides/components/Rides.js
+++ src/rides/components/Rides.js
@@ -38,6 +38,7 @@ class Rides extends React.Component {
       individualRide = rides.map(ride => {
         const { id, name, photo_url, description, distance, points } = ride
 
+
         const finishData = {
           user_id: this.props.user.id,
           ride_id: id,
@@ -52,13 +53,14 @@ class Rides extends React.Component {
           rideParams: {id, name, photo_url, description, distance, points, user}
         }
 
+
         return (
           <div className="ride-div" key={id}>
             <h1><Link to={detail_link} user={user} replace>Ride: {name}</Link></h1>
             <p>{description}</p>
             <p>Distance: {distance}</p>
             <p>Points: {points}</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39196.08315236001!2d-71.23622179902506!3d42.38258817341337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e3782b52ba7537%3A0x515285872b86c6c8!2sCharles+River+Park+1!5e0!3m2!1sen!2sus!4v1544048434412" style={{border:0, width:'100%', height:450, frameBorder:0}} allowFullScreen></iframe>
+            <iframe src={photo_url} style={{border:0, width:'100%', height:450, frameBorder:0}} allowFullScreen></iframe>
             <FinishNew user={user} {...finishData}/>
             <br />
           </div>
